98f423f52134aa94a61d0fa3eda4bd5979d09c4a
91jzhu <j1712032377@163.com>
2022-02-16 22:41:35 +0800

    每日图表基本完成，准备制作下拉菜单

--- src/components/HomePage/Record.vue
+++ src/components/HomePage/Record.vue
@@ -8,7 +8,12 @@
       购买记录
     </span>
     </header>
-    <div class="chart" ref="chartRef"/>
+
+    // 下拉菜单，选择日期
+
+    <div class="chartWrapper">
+      <div class="chart" ref="chartRef"/>
+    </div>
   </div>
 </template>
 
@@ -44,49 +49,57 @@ export default {
               }
             }
           ],
-          grid:{
-            left:'2%',
-            right:'1%',
-            bottom:'10%',
-            containLabel:true
+          title:{
+            show:true,
+            text:'2022年02月16日消费情况',
+            top:'top',
+            left:'center',
+            textStyle:{
+              color:'grey',
+              fontSize:22,
+              fontStyle:'oblique',
+              width:100
+            }
+          },
+          grid: {
+            left: '2%',
+            right: '1%',
+            containLabel: true
           },
           xAxis: {
             type: 'category',
             axisLabel: {
               interval: 0,
               rotate: 30,
-              margin:16,
-              fontSize:20
+              margin: 16,
+              fontSize: 20
             },
-            axisTick:{
-              show:false,
-              alignWithLabel:true
+            axisTick: {
+              show: false,
+              alignWithLabel: true
             }
           },
           tooltip: {},
           legend: {},
           yAxis: {
-            axisLabel:{
-              fontSize:18,
-              showMinLabel:false,
+            axisLabel: {
+              fontSize: 18,
+              showMinLabel: false,
+            }
+          },
+          label: {
+            show: true,		//开启显示
+            position: 'top',	//在上方显示
+            fontSize: 18,
+            textStyle: {	    //数值样式
+              color: 'black',
+              fontSize: 18
             }
           },
           series: {
             type: 'bar',
             encode: {x: 'name', y: 'score'},
             datasetIndex: 1,
-            itemStyle: {
-              normal: {
-                label: {
-                  show: true,		//开启显示
-                  position: 'top',	//在上方显示
-                  textStyle: {	    //数值样式
-                    color: 'black',
-                    fontSize: 16
-                  }
-                }
-              }
-            }
           }
         })
       }
@@ -100,6 +113,9 @@ export default {
 .wrapper {
   height: 100vh;
   overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 
   header {
     border-bottom: 1px solid grey;
@@ -122,8 +138,16 @@ export default {
     }
   }
 
-  .chart {
-    height: 80vh;
+  .chartWrapper {
+    //border:1px solid blue;
+    .chart {
+      border: 1px solid grey;
+      border-top-left-radius: 24px;
+      border-top-right-radius: 24px;
+      padding-top: 12px;
+      height: 72vh;
+    }
   }
+
 }
 </style>
---
+++ src/components/Tool/DropDown-Item.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "DropDown-Item"
+}
+</script>
+
+<style scoped>
+
+</style>
